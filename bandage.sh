0e21917fb7c534ec80e2c00285edaae21e0ebf54
P.Merrick <p.merrick@optergy.com>
2018-04-10 09:46:42 +0930

    no message

--- bandage.sh
+++ bandage.sh
@@ -61,8 +61,8 @@ wait
 
 echo "removing backdoor"
 
-rm -rf /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/com/optergy/web/action/tools/Console.class
-rm -rf /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/com/optergy/web/action/tools/ajax/Console*
+#rm -rf /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/com/optergy/web/action/tools/Console.class
+#rm -rf /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/com/optergy/web/action/tools/ajax/Console*
 
-cat /tmp/result.txt ; sudo invoke-rc.d tomcat restart &
+cat /tmp/result.txt && sudo invoke-rc.d tomcat restart &
 wait
